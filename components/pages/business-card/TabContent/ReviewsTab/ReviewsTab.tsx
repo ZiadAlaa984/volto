ed2969ad26c0801ad3c77832788a9e0b8dfd8d8f
ziad alaa <ziadalaa984@gmail.com>
2026-04-11 20:27:47 +0200

    comment task reviews

--- components/pages/business-card/TabContent/ReviewsTab/ReviewsTab.tsx
+++ components/pages/business-card/TabContent/ReviewsTab/ReviewsTab.tsx
@@ -1,6 +1,30 @@
 import React from 'react'
 
 function ReviewsTab() {
+    // create a table reviews
+    // example data
+    const reviews = [
+        {
+            id: 1,
+            Customer: "John Doe",
+            rating: 5,
+            Review: "Great service",
+            created_at: "2022-01-01",
+        },
+        {
+            id: 2,
+            Customer: "Jane Doe",
+            rating: 4,
+            Review: "Good service",
+            created_at: "2022-01-01",
+        },
+    ]
+    // create table shadcn component
+    // link table from shoogle 
+    https://shoogle.dev/search?q=table&tab=search&preview=https%3A%2F%2Fwww.shadcnblocks.com%2Fexplorer%2Fcomponents%3Fgroup%3Dtable%26gridSize%3D2
+    // Customer, rating, review, time, action (delete);
+
+
     return (
         <div>ReviewsTab</div>
     )
